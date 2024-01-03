106eab0df81099cc7154dad1f71978b873e6e0a9
kbandison <kbandison@gmail.com>
2024-01-03 03:50:20 -0500

    started basics for site

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+// import Typed from "react-typed";
+import { Link } from "react-router-dom";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="">
+      <h1>Cooking in Spirit!</h1>
+      <h4>Get your plate now!!!!</h4>
+      <button>Buy Now!</button>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Hero from "../Components/Hero";
+
+const Dashboard = () => {
+  return (
+    <div className="screen">
+      <Hero />
+    </div>
+  );
+};
+
+export default Dashboard;
