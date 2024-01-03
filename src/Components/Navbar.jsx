bfaea769ae943cd01e3242e6b63199f528d5962e
kbandison <kbandison@gmail.com>
2024-01-03 12:17:13 -0500

    added beginning of dashboard

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -50,7 +50,7 @@ export default function Nav() {
                     alt="Your Company"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block sm:w-full border">
+                <div className="hidden sm:ml-6 sm:block sm:w-full">
                   {/* Main menu buttons */}
 
                   <div className="flex space-x-4 justify-center">
@@ -72,18 +72,21 @@ export default function Nav() {
                   </div>
                 </div>
               </div>
-              {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div>
+                  <h3>Cart (1)</h3>
+                </div>
+                {/* <button
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+                </button> */}
 
-                Profile dropdown
-                <Menu as="div" className="relative ml-3">
+                {/* Profile dropdown */}
+                {/* <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
@@ -146,8 +149,8 @@ export default function Nav() {
                       </Menu.Item>
                     </Menu.Items>
                   </Transition>
-                </Menu>
-              </div> */}
+                </Menu> */}
+              </div>
             </div>
           </div>
 
